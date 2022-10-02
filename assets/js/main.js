2171dc75645858efc2850f276f7e5d601cd127aa
kevin5806 <kevinleoni58@gmail.com>
2022-10-03 00:10:27 +0200

    added win highlight

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,40 +64,88 @@ function tris (box) {
             //X
             if (b1 == 1 && b2 == 1 && b3 == 1) {
                 vx = true;
+                document.getElementById("bx1").src="assets/images/x-bw.png";
+                document.getElementById("bx2").src="assets/images/x-bw.png";
+                document.getElementById("bx3").src="assets/images/x-bw.png";
             } else if (b4 == 1 && b5 == 1 && b6 == 1) {
                 vx = true;
+                document.getElementById("bx4").src="assets/images/x-bw.png";
+                document.getElementById("bx5").src="assets/images/x-bw.png";
+                document.getElementById("bx6").src="assets/images/x-bw.png";
             } else if (b7 == 1 && b8 == 1 && b9 == 1) {
                 vx = true;
+                document.getElementById("bx7").src="assets/images/x-bw.png";
+                document.getElementById("bx8").src="assets/images/x-bw.png";
+                document.getElementById("bx9").src="assets/images/x-bw.png";
             } else if (b1 == 1 && b4 == 1 && b7 == 1) {
                 vx = true;
+                document.getElementById("bx1").src="assets/images/x-bw.png";
+                document.getElementById("bx4").src="assets/images/x-bw.png";
+                document.getElementById("bx7").src="assets/images/x-bw.png";
             } else if (b2 == 1 && b5 == 1 && b8 == 1) {
                 vx = true;
+                document.getElementById("bx2").src="assets/images/x-bw.png";
+                document.getElementById("bx5").src="assets/images/x-bw.png";
+                document.getElementById("bx8").src="assets/images/x-bw.png";
             } else if (b3 == 1 && b6 == 1 && b9 == 1) {
                 vx = true;
+                document.getElementById("bx3").src="assets/images/x-bw.png";
+                document.getElementById("bx6").src="assets/images/x-bw.png";
+                document.getElementById("bx9").src="assets/images/x-bw.png";
             } else if (b1 == 1 && b5 == 1 && b9 == 1) {
                 vx = true;
+                document.getElementById("bx1").src="assets/images/x-bw.png";
+                document.getElementById("bx5").src="assets/images/x-bw.png";
+                document.getElementById("bx9").src="assets/images/x-bw.png";
             } else if (b3 == 1 && b5 == 1 && b7 == 1) {
                 vx = true;
+                document.getElementById("bx3").src="assets/images/x-bw.png";
+                document.getElementById("bx5").src="assets/images/x-bw.png";
+                document.getElementById("bx7").src="assets/images/x-bw.png";
             } else {
                 if (turn == 9) {
                     pare = true;
                 } else {
                     if (b1 == 2 && b2 == 2 && b3 == 2) {
                         vo = true;
+                        document.getElementById("bo1").src="assets/images/o-bw.png";
+                        document.getElementById("bo2").src="assets/images/o-bw.png";
+                        document.getElementById("bo3").src="assets/images/o-bw.png";
                     } else if (b4 == 2 && b5 == 2 && b6 == 2) {
                         vo = true;
+                        document.getElementById("bo4").src="assets/images/o-bw.png";
+                        document.getElementById("bo5").src="assets/images/o-bw.png";
+                        document.getElementById("bo6").src="assets/images/o-bw.png";
                     } else if (b7 == 2 && b8 == 2 && b9 == 2) {
                         vo = true;
+                        document.getElementById("bo7").src="assets/images/o-bw.png";
+                        document.getElementById("bo8").src="assets/images/o-bw.png";
+                        document.getElementById("bo9").src="assets/images/o-bw.png";
                     } else if (b1 == 2 && b4 == 2 && b7 == 2) {
                         vo = true;
+                        document.getElementById("bo1").src="assets/images/o-bw.png";
+                        document.getElementById("bo4").src="assets/images/o-bw.png";
+                        document.getElementById("bo7").src="assets/images/o-bw.png";
                     } else if (b2 == 2 && b5 == 2 && b8 == 2) {
                         vo = true;
+                        document.getElementById("bo2").src="assets/images/o-bw.png";
+                        document.getElementById("bo5").src="assets/images/o-bw.png";
+                        document.getElementById("bo8").src="assets/images/o-bw.png";
                     } else if (b3 == 2 && b6 == 2 && b9 == 2) {
                         vo = true;
+                        document.getElementById("bo3").src="assets/images/o-bw.png";
+                        document.getElementById("bo6").src="assets/images/o-bw.png";
+                        document.getElementById("bo9").src="assets/images/o-bw.png";
                     } else if (b1 == 2 && b5 == 2 && b9 == 2) {
                         vo = true;
+                        document.getElementById("bo1").src="assets/images/o-bw.png";
+                        document.getElementById("bo5").src="assets/images/o-bw.png";
+                        document.getElementById("bo9").src="assets/images/o-bw.png";
                     } else if (b3 == 2 && b5 == 2 && b7 == 2) {
                         vo = true;
+                        document.getElementById("bo3").src="assets/images/o-bw.png";
+                        document.getElementById("bo5").src="assets/images/o-bw.png";
+                        document.getElementById("bo7").src="assets/images/o-bw.png";
                     }
             }
         }
