e895b21dc3ece45b454b256a434f253787a7024a
kevin5806 <kevinleoni58@gmail.com>
2022-10-02 18:28:17 +0200

    bugfix

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,113 +6,115 @@ function reloadpage () {
 }
 
 function tris (box) {
-    switch (box) {
-        case 1: if (b1 != 0) { risposta = false; } else { risposta = true; } break;
-        case 2: if (b2 != 0) { risposta = false; } else { risposta = true; } break;
-        case 3: if (b3 != 0) { risposta = false; } else { risposta = true; } break;
-        case 4: if (b4 != 0) { risposta = false; } else { risposta = true; } break;
-        case 5: if (b5 != 0) { risposta = false; } else { risposta = true; } break;
-        case 6: if (b6 != 0) { risposta = false; } else { risposta = true; } break;
-        case 7: if (b7 != 0) { risposta = false; } else { risposta = true; } break;
-        case 8: if (b8 != 0) { risposta = false; } else { risposta = true; } break;
-        case 9: if (b9 != 0) { risposta = false; } else { risposta = true; } break;
-    }
-  
+    if (vx == false && vo == false && pare == false) {
+        switch (box) {
+            case 1: if (b1 != 0) { risposta = false; } else { risposta = true; } break;
+            case 2: if (b2 != 0) { risposta = false; } else { risposta = true; } break;
+            case 3: if (b3 != 0) { risposta = false; } else { risposta = true; } break;
+            case 4: if (b4 != 0) { risposta = false; } else { risposta = true; } break;
+            case 5: if (b5 != 0) { risposta = false; } else { risposta = true; } break;
+            case 6: if (b6 != 0) { risposta = false; } else { risposta = true; } break;
+            case 7: if (b7 != 0) { risposta = false; } else { risposta = true; } break;
+            case 8: if (b8 != 0) { risposta = false; } else { risposta = true; } break;
+            case 9: if (b9 != 0) { risposta = false; } else { risposta = true; } break;
+        }
+    
 
-    if (risposta == true) {
-        turn++;
-        
-        document.getElementById("turnCount").innerHTML = "È il turno  " + turn;
+        if (risposta == true) {
+            turn++;
+            
+            document.getElementById("turnCount").innerHTML = "È il turno  " + turn;
 
-        if (turn % 2 == 0) {
-            document.getElementById('bo'+box).classList.remove("none");
-            switch (box) {
-                case 1: b1 = 2; break;
-                case 2: b2 = 2; break;
-                case 3: b3 = 2; break;
-                case 4: b4 = 2; break;
-                case 5: b5 = 2; break;
-                case 6: b6 = 2; break;
-                case 7: b7 = 2; break;
-                case 8: b8 = 2; break;
-                case 9: b9 = 2; break;
-            }
-            document.getElementById("message").innerHTML = "È il turno della X";
-        
+            if (turn % 2 == 0) {
+                document.getElementById('bo'+box).classList.remove("none");
+                switch (box) {
+                    case 1: b1 = 2; break;
+                    case 2: b2 = 2; break;
+                    case 3: b3 = 2; break;
+                    case 4: b4 = 2; break;
+                    case 5: b5 = 2; break;
+                    case 6: b6 = 2; break;
+                    case 7: b7 = 2; break;
+                    case 8: b8 = 2; break;
+                    case 9: b9 = 2; break;
+                }
+                document.getElementById("message").innerHTML = "È il turno della X";
+            
+            } else {
+                document.getElementById('bx'+box).classList.remove("none");
+                switch (box) {
+                    case 1: b1 = 1; break;
+                    case 2: b2 = 1; break;
+                    case 3: b3 = 1; break;
+                    case 4: b4 = 1; break;
+                    case 5: b5 = 1; break;
+                    case 6: b6 = 1; break;
+                    case 7: b7 = 1; break;
+                    case 8: b8 = 1; break;
+                    case 9: b9 = 1; break;
+                }
+                document.getElementById("message").innerHTML = "È il tuorno della O";
+            }   
+
         } else {
-            document.getElementById('bx'+box).classList.remove("none");
-            switch (box) {
-                case 1: b1 = 1; break;
-                case 2: b2 = 1; break;
-                case 3: b3 = 1; break;
-                case 4: b4 = 1; break;
-                case 5: b5 = 1; break;
-                case 6: b6 = 1; break;
-                case 7: b7 = 1; break;
-                case 8: b8 = 1; break;
-                case 9: b9 = 1; break;
-            }
-            document.getElementById("message").innerHTML = "È il tuorno della O";
-        }   
+            document.getElementById("message").innerHTML = "Casella Occupata";
+        }
 
-    } else {
-        document.getElementById("message").innerHTML = "Casella Occupata";
-    }
-
-    //controllo vincita
-    if (turn == 9) {
-        pare = true;
-    } else {
-        //X
-        if (b1 == 1 && b2 == 1 && b3 == 1) {
-            vx = true;
-        } else if (b4 == 1 && b5 == 1 && b6 == 1) {
-            vx = true;
-        } else if (b7 == 1 && b8 == 1 && b9 == 1) {
-            vx = true;
-        } else if (b1 == 1 && b4 == 1 && b7 == 1) {
-            vx = true;
-        } else if (b2 == 1 && b5 == 1 && b8 == 1) {
-            vx = true;
-        } else if (b3 == 1 && b6 == 1 && b9 == 1) {
-            vx = true;
-        } else if (b1 == 1 && b5 == 1 && b9 == 1) {
-            vx = true;
-        } else if (b3 == 1 && b5 == 1 && b7 == 1) {
-            vx = true;
-        } else {
-            if (b1 == 2 && b2 == 2 && b3 == 2) {
-                vo = true;
-            } else if (b4 == 2 && b5 == 2 && b6 == 2) {
-                vo = true;
-            } else if (b7 == 2 && b8 == 2 && b9 == 2) {
-                vo = true;
-            } else if (b1 == 2 && b4 == 2 && b7 == 2) {
-                vo = true;
-            } else if (b2 == 2 && b5 == 2 && b8 == 2) {
-                vo = true;
-            } else if (b3 == 2 && b6 == 2 && b9 == 2) {
-                vo = true;
-            } else if (b1 == 2 && b5 == 2 && b9 == 2) {
-                vo = true;
-            } else if (b3 == 2 && b5 == 2 && b7 == 2) {
-                vo = true;
+        //controllo vincita
+            //X
+            if (b1 == 1 && b2 == 1 && b3 == 1) {
+                vx = true;
+            } else if (b4 == 1 && b5 == 1 && b6 == 1) {
+                vx = true;
+            } else if (b7 == 1 && b8 == 1 && b9 == 1) {
+                vx = true;
+            } else if (b1 == 1 && b4 == 1 && b7 == 1) {
+                vx = true;
+            } else if (b2 == 1 && b5 == 1 && b8 == 1) {
+                vx = true;
+            } else if (b3 == 1 && b6 == 1 && b9 == 1) {
+                vx = true;
+            } else if (b1 == 1 && b5 == 1 && b9 == 1) {
+                vx = true;
+            } else if (b3 == 1 && b5 == 1 && b7 == 1) {
+                vx = true;
+            } else {
+                if (turn == 9) {
+                    pare = true;
+                } else {
+                    if (b1 == 2 && b2 == 2 && b3 == 2) {
+                        vo = true;
+                    } else if (b4 == 2 && b5 == 2 && b6 == 2) {
+                        vo = true;
+                    } else if (b7 == 2 && b8 == 2 && b9 == 2) {
+                        vo = true;
+                    } else if (b1 == 2 && b4 == 2 && b7 == 2) {
+                        vo = true;
+                    } else if (b2 == 2 && b5 == 2 && b8 == 2) {
+                        vo = true;
+                    } else if (b3 == 2 && b6 == 2 && b9 == 2) {
+                        vo = true;
+                    } else if (b1 == 2 && b5 == 2 && b9 == 2) {
+                        vo = true;
+                    } else if (b3 == 2 && b5 == 2 && b7 == 2) {
+                        vo = true;
+                    }
             }
         }
-    }
 
-    //azioni vincita
-    if (pare == true) {
-        document.getElementById("turnCount").innerHTML = "Pareggio";
-        document.getElementById("message").classList.add("none");
-        document.getElementById("reloadBtn").classList.remove("none");
-    } else if (vx == true) {
-        document.getElementById("turnCount").innerHTML = "Vincitore X";
-        document.getElementById("message").classList.add("none");
-        document.getElementById("reloadBtn").classList.remove("none");
-    } else if (vo == true) {
-        document.getElementById("turnCount").innerHTML = "Vincitore O";
-        document.getElementById("message").classList.add("none");
-        document.getElementById("reloadBtn").classList.remove("none");
+        //azioni vincita
+        if (vx == true) {
+            document.getElementById("turnCount").innerHTML = "Vincitore X";
+            document.getElementById("message").classList.add("none");
+            document.getElementById("reloadBtn").classList.remove("none");
+        } else if (pare == true) {
+            document.getElementById("turnCount").innerHTML = "Pareggio";
+            document.getElementById("message").classList.add("none");
+            document.getElementById("reloadBtn").classList.remove("none"); 
+        } else if (vo == true) {
+            document.getElementById("turnCount").innerHTML = "Vincitore O";
+            document.getElementById("message").classList.add("none");
+            document.getElementById("reloadBtn").classList.remove("none");
+        }
     }
 }
